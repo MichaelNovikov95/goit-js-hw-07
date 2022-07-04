db70c1bae2ca5eabd528c8a9581ef1f66eb15f02
Mykhailo Novikov <100936797+MichaelNovikov95@users.noreply.github.com>
2022-07-04 19:07:02 +0300

    homework

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const boxForGalleryCards = document.querySelector('.gallery');
+
+
+// Создаём разметку с импорта
+function addGalleryCards (galleryItems) {
+  const GalleryCards = galleryItems.map(({preview, original,description}) => {
+    return `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`
+  }).join('');
+
+  return GalleryCards;
+};
+// Добавляем разметку на страницу
+boxForGalleryCards.insertAdjacentHTML('beforeend', addGalleryCards(galleryItems));
+
+// Добавляем библиотеку
+const galleryHandleClick = (event) => {
+event.preventDefault();
+
+const originalImgRef = event.target.dataset.source;
+const imgAltRef = event.target.alt;
+
+if (event.target.nodeName !== 'IMG') {
+  return;
+}
+
+const instance = basicLightbox.create(`
+<img src='${originalImgRef}' alt ='${imgAltRef}' >
+`,).show();
+};
+
+boxForGalleryCards.addEventListener('click', galleryHandleClick);
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,23 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+const captionRef = 'CAPTION!!!'
+
+function addGalleryMarkup (galleryItems) {
+  const cardsMarkUp = galleryItems.map(({preview, original, description}) => {
+    return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" 
+    src="${preview}" 
+    alt="${description}" />
+  </a>`
+  }).join('');
+
+  return cardsMarkUp;
+}
+
+galleryRef.insertAdjacentHTML('beforeend', addGalleryMarkup (galleryItems));
+
+
+let lightbox = new SimpleLightbox('.gallery a', {captions:true , captionsData: 'alt', captionDelay:250});
